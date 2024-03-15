2fbf190f00b035ebbbd8878b961299d1b3ddee07
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2024-03-15 14:57:32 +0530

    add filters and route config

--- src/main/java/com/prathamesh/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/prathamesh/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.prathamesh.cruddemo.dao;
 
 import com.prathamesh.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // That's it ... no need to write any code LOL!
 }
